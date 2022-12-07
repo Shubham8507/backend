c741c25419855f66e7a466ec08596d94d1899d9a
Shubham8507 <52946928+Shubham8507@users.noreply.github.com>
2022-12-07 13:27:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+var cors = require('cors');
+const Stripe = require('stripe');
+const stripe = Stripe("your server key");
+
+const app = express();
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+
+
+app.get("/serverStart", async (req, res) => {
+    console.log(req.body);
+    res.send("stripe server Start");
+});
+
+app.post("/checkout", async (req, res) => {
+    console.log(req.body);
+    const items = req.body.items;
+    let lineItems = [];
+    items.forEach((item) => {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
+
+    const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: "http://localhost:3000/success",
+        cancel_url: "http://localhost:3000/cancel"
+    });
+
+    res.send(JSON.stringify({
+        url: session.url
+    }));
+});
+
+app.listen(4000, () => console.log("Listening on port 4000!"));
